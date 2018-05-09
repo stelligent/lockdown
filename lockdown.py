a540e607042e6b09974bf950ccf9f706c0906728
Fernando J Pando <fernando.pando@stelligent.com>
2018-05-09 15:09:29 -0400

    set-nacls

--- lockdown.py
+++ lockdown.py
@@ -1,21 +1,82 @@
+import sys
 import boto3
+import jinja2
 
-# check to make sure root account
 
-1. cut off all network access to all subnets
-  - security group rules are zeroed out
-  - acls are applied to prevent any and all traffic
 
-2. deactivates all iam users and deletes roles
-  - deactivates all iam user accounts
-  - deletes all iam roles, which could be used for cross-account access, or as part of a future event,
-      such as a cron'd lambda or scheduled datapipeline
+print("Verify current user has Administrator privileges.")
+iam_client = boto3.client('iam')
+user_name = iam_client.get_user()['User']['UserName']
+user_policies = iam_client.list_attached_user_policies(UserName=user_name)['AttachedPolicies']
+for policy in user_policies:
+  if policy['PolicyName'] == "AdministratorAccess":
+    user_admin = True
+if user_admin:
+  print("current aws profile user keys are valid.  continuing.")
+else:
+  print("In order to run this code, please add the AdministratorAccess managed IAM policy to your current user.")
+  sys.exit(1)
 
-3. ebs snapshot all instances
-  - all instance volumes are snapshotted for any future forensics
 
-4. execute any forensic tooling via SSM
-  - attempt to capture running processes and system memory
 
-5. report on cloudtrail and flowlogs status
-  - if logs are available, print out location of logs
+
+if (len(sys.argv) == 1):
+
+  print("Network Access Control List Deny.")
+  ec2_client = boto3.client('ec2')
+  nacls = ec2_client.describe_network_acls()['NetworkAcls']
+  for nacl in nacls:
+    ec2_client.create_network_acl_entry(
+      CidrBlock='0.0.0.0/0',
+      Egress=True,
+      Protocol='-1',
+      RuleAction='deny',
+      RuleNumber=1,
+      NetworkAclId=nacl['NetworkAclId']
+    )
+    ec2_client.create_network_acl_entry(
+      CidrBlock='0.0.0.0/0',
+      Egress=False,
+      Protocol='-1',
+      RuleAction='deny',
+      RuleNumber=2,
+      NetworkAclId=nacl['NetworkAclId']
+    )
+
+
+  print("  - deny policy jinja template is attached to all users, groups and roles.")
+  print("    -  exempt this user: " + user_name + " from deny policy addition")
+
+  print("  - all instances and mount volumes are snapshotted.")
+
+  print("  - add bucket policy jinja template to disable all public reads and writes.")
+
+  print("  - attempt to capture running processes and system memory via SSM, if available.")
+
+  print("  - stop all instances.")
+  
+  print("  - lookup and print cloudtrail logs and vpc flowlogs location, if available.")
+
+
+elif (sys.argv[1] == "revert"):
+  print("Network Access Control List Allow.")
+  ec2_client = boto3.client('ec2')
+  nacls = ec2_client.describe_network_acls()['NetworkAcls']
+  for nacl in nacls:
+    ec2_client.delete_network_acl_entry(
+      Egress=True,
+      RuleNumber=1,
+      NetworkAclId=nacl['NetworkAclId']
+    )
+    ec2_client.delete_network_acl_entry(
+      Egress=False,
+      RuleNumber=2,
+      NetworkAclId=nacl['NetworkAclId']
+    )
+
+
+  print("  - remove network deny ACLs, if existing")
+
+  print("  - Deny policy applied to users, groups, and roles is removed -- tags on policies not yet supported?")
+
+  print("  - Remove deny public access policy applied to s3 buckets is removes")
